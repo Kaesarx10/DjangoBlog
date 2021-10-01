f730a2fedd39f1ee6b9a18d6eaf71c8980019991
kaesarx10 <venzor.ca@gmail.com>
2021-09-30 19:09:44 -0500

    This is my first blog.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^ns+ryo$+yz7m!*s76h5e2c*q*#^aa=*kbi!z39$7o71c@qc05'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
